eae36b9ec4f3f472a5f7d6a83a02944a39f19869
Tomasz Wirkus <t.wirkus@i4b.pl>
2021-05-16 16:36:53 +0200

    RAT-8 Added Rotation funcionality

--- app/src/main/java/com/tomasz/animacja/MainActivity.java
+++ app/src/main/java/com/tomasz/animacja/MainActivity.java
@@ -18,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void obracaj(View v){
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.obrot);
+        ImageView iv = (ImageView)findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+
+
     public void skaluj(View v) {
         Animation a = AnimationUtils.loadAnimation(this, R.anim.skala);
         ImageView iv = (ImageView) findViewById(R.id.imageView);
