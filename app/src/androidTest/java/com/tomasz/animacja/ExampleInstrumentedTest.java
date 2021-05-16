119beab7dc94f4446d6093df83b843b6df94f7a8
Radosaw Grosz <radekgrosz@tlen.pl>
2021-05-16 15:36:29 +0200

    empty project

--- app/src/androidTest/java/com/tomasz/animacja/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tomasz/animacja/ExampleInstrumentedTest.java
@@ -1,8 +1,9 @@
-package com.example.transormacjeafiniczne;
+package com.tomasz.animacja;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.transormacjeafiniczne", appContext.getPackageName());
+        assertEquals("com.tomasz.animacja", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/tomasz/animacja/MainActivity.java
+++ app/src/main/java/com/tomasz/animacja/MainActivity.java
@@ -1,7 +1,12 @@
-package com.example.transormacjeafiniczne;
+package com.tomasz.animacja;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
